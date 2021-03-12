e609dabfcc5f9761f399d4cf31cde723f5deff53
Hekera <40907168+Hekera@users.noreply.github.com>
2021-03-12 01:01:35 -0500

    Update ezpzspritesplitter.py

--- ezpzspritesplitter.py
+++ ezpzspritesplitter.py
@@ -46,8 +46,8 @@ class SpriteGUI(Frame):
 	def __init__(self, parent, image, index, name_list=[], exclude=False):
 		Frame.__init__(self, parent)
 		
-		self.image = image
-		self.thumbnail = ImageTk.PhotoImage(self.image.resize(get_resize(), NEAREST))
+		self.sprite = image
+		self.thumbnail = ImageTk.PhotoImage(self.sprite.resize(get_resize(), NEAREST))
 		self.names = name_list
 		self.default = f"{index[0]}-{index[1]}"
 		self.exclude = exclude
@@ -62,16 +62,35 @@ class SpriteGUI(Frame):
 		#button.bind("<MouseWheel>", on_workspace_mousewheel)
 	
 	def __dict__(self):
-		return {"id": self.default, "names": self.names, "exclude": self.exclude}
+		return {"id": self.default, "exclude": self.exclude, "names": self.names}
+	
+	def update_label(self, delimiter):
+		if sprite.exclude:
+				self.label["text"] = ""
+			else:
+				self.label["text"] = get_label(delimiter)
+				
+	
+	def get_label(self, delimiter):
+		if len(self.names) == 0:
+			return sprite.default
+		else: 
+			new_label = ""
+			for name in self.names:
+				new_label += name + delimiter
+			return new_label[:new_label.rindex(delimiter)]
 	
 class Workspace(VScrollable):
-	def __init__(self, parent, image):
-		VScrollable.__init__(self, parent, tile_size, names_list=[])
+	def __init__(self, parent, image, tile_size, names_list=[]):
+		VScrollable.__init__(self, parent)
 		
 		self.image = image
 		self.tile_width = tile_size[0]
 		self.tile_height = tile_size[1]
 		self.names_list = names_list
+		self.selected_name_index = -1
+		self.exclude_mode = False
+		self.delete_mode = False
 		
 		self.hbar = Scrollbar(self, orient=HORIZONTAL, command=self.canvas.xview)
 		self.canvas.config(xscrollcommand=hbar.set)
@@ -84,34 +103,130 @@ class Workspace(VScrollable):
 		def get_coords(x_index, y_index):
 			return ((x_index-1)*self.tile_width, (y_index-1)*self.tile_height, x_index*self.tile_width, y_index*self.tile_height)
 		
-		grid = []
+		#grid = []
 		for i in range(0, int(self.image.height/self.tile_height)+1):
-			if i > 0:
-				row = []
+			#if i > 0:
+			#	row = []
 			for j in range(0, int(self.image.width/self.tile_width)+1):
-				frame = Frame(workspace, bg=self.bg_colors[1], borderwidth=1, relief="solid")
-				frame.grid(row=i, column=j, sticky="n")
-				frame.bind("<MouseWheel>", on_workspace_mousewheel)
+				#frame = Frame(self.frame, bg=self.bg_colors[1], borderwidth=1, relief="solid")
+				#frame.grid(row=i, column=j, sticky="n")
+				#frame.bind("<MouseWheel>", on_workspace_mousewheel)
 				if j > 0 and i > 0:
-					sprite_gui = SpriteGUI(self, self.image.crop(box=get_coords(j, i)), (i,j))
-					row.append(sprite_gui)
-					
+					SpriteGUI(self.frame, self.image.crop(box=get_coords(j, i)), (i,j)).grid(row=i, column=j, sticky="n")
+					#row.append(sprite_gui)
 				elif j == 0 and i > 0:
-					button = Button(frame, text="",command=lambda x=i-1: on_sprite_click_row(x), fg=self.fg_color, bg=self.bg_colors[0],width=2,height=(6*get_resize()[1])//self.tile_display_size)
-					button.bind("<MouseWheel>", on_workspace_mousewheel)
-					button.pack()
+					button = Button(self.frame, text="",command=lambda x=i: self.on_click_row(x), fg=self.fg_color, bg=self.bg_colors[0],width=2,height=(6*get_resize()[1])//self.tile_display_size)
+					#button.bind("<MouseWheel>", on_workspace_mousewheel)
+					button..grid(row=i, column=j, sticky="w")
 				elif i == 0 and j > 0:
-					button = Button(frame, text="",command=lambda x=j-1: on_sprite_click_col(x), fg=self.fg_color, bg=self.bg_colors[0],width=(10*get_resize()[0])//self.tile_display_size,height=1)
-					button.bind("<MouseWheel>", on_workspace_mousewheel)
-					button.pack()
-			if i > 0:
-				tiles.append(tuple(row))
+					button = Button(self.frame, text="",command=lambda x=j: self.on_click_col(x), fg=self.fg_color, bg=self.bg_colors[0],width=(10*get_resize()[0])//self.tile_display_size,height=1)
+					#button.bind("<MouseWheel>", on_workspace_mousewheel)
+					button.grid(row=i, column=j, sticky="n")
+			#if i > 0:
+			#	tiles.append(tuple(row))
 			
-		self.sprites = tuple(tiles)
+		#self.sprites = tuple(tiles)
+	
+	def on_sprite_click(self, row_index, col_index):
+		sprite = self.frame.grid_slaves(row=row_index, column=col_index)[0]
+		if self.exclude_mode:
+			sprite.exclude = not sprite.exclude
+			sprite.update_label()
+		elif self.delete_mode:
+			sprite.names = []
+			sprite.update_label()
+		elif self.selected_name_index > -1:
+			sprite.names.append(name_list[self.selected_name_index])
+			sprite.update_label()
+	
+	def on_click_col(self, index):
+		sprites = self.frame.grid_slaves(column=index)
+		if self.exclude_mode:
+			exclude_all = True
+			for sprite in sprites:
+				if type(sprite) == Frame:
+					exclude_all = exclude_all and sprite.exclude
+			for sprite in sprites:
+				if type(sprite) == Frame:
+					sprite.exclude = not exclude_all
+					sprite.update_label()
+		else:
+			for i in range(1, len(sprites)):
+				on_sprite_click(i, index)
+
+	def on_click_row(self, index):
+		sprites = self.frame.grid_slaves(row=index)
+		sprites.pop(len(sprites)-1)
+		if self.exclude_mode:
+			exclude_all = True
+			for sprite in sprites:
+				if type(sprite) == Frame:
+					exclude_all = exclude_all and sprite.exclude
+			for sprite in sprites:
+				if type(sprite) == Frame:
+					sprite.exclude = not exclude_all
+					sprite.update_label()
+		else:
+			for i in range(1, len(sprites)):
+				on_sprite_click(index, i)
+	
+	def update_file_labels():
+		sprites = self.frame.grid_slaves()
+		for row in self.sprites:
+			for sprite in row:
+				update_file_label(sprite)
+
+class Panel(Frame):
+	def __init__(self, parent):
+		Frame.__init__(self, parent)
+		delimiter_input = Frame(panel, bg=self.bg_colors[0])
+		delimiter_input.pack(padx=5, pady=10)
+		mode_input = Frame(panel, bg=self.bg_colors[0])
+		mode_input.pack(padx=5, pady=5)
+		Label(panel, text="Names", fg=self.fg_color, bg=self.bg_colors[0], font=("TkDefaultFont", 14)).pack(padx=5, pady=5)
+		name_input = Frame(panel, bg=self.bg_colors[0])
+		name_input.pack(padx=5)
+		#names list scrollable object
+		submit_input = Frame(panel, bg=self.bg_colors[0])
+		submit_input.pack(side=BOTTOM, padx=5, pady=10)
+		folder_input = Frame(panel, bg=self.bg_colors[0])
+		folder_input.pack(side=BOTTOM, padx=5)
+		
+		Label(delimiter_input, text="Delimiter:", fg=self.fg_color, bg=self.bg_colors[0]).pack(side=LEFT)
+		delimiter = StringVar(value="_")
+		delimiter.trace_add("write", lambda name, index, mode: update_file_labels())
+		delimiter_entry = Entry(delimiter_input, textvariable=delimiter, fg=self.fg_color, bg=self.bg_colors[2], width=4, relief=FLAT)
+		delimiter_entry.pack(side=LEFT)
+		
+		exclude_toggle = Button(mode_input, text="Exclude Mode", command=toggle_exclude_mode, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT)
+		exclude_toggle.pack(side=LEFT, padx=5)
+		delete_toggle = Button(mode_input, text="Clear Mode", command=toggle_delete_mode, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT)
+		delete_toggle.pack(side=LEFT, padx=5)
+		exclude_mode = [False, exclude_toggle]
+		delete_mode = [False, delete_toggle]
+		
+		name_entry = Entry(name_input, fg=self.fg_color, bg=self.bg_colors[2], relief=FLAT)
+		name_entry.bind('<Return>', add_name)
+		name_entry.pack(side=LEFT)
+		Button(name_input, text="Add Name",command=add_name, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack(side=LEFT, padx=5)
+		
+		folder_name = Label(folder_input, text="No directory selected.", fg=self.fg_color, bg=self.bg_colors[0], wraplength=180)
+		folder_name.pack()
+		Button(folder_input, text="Browse...",command=open_folder, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack()
+		
+		error = Label(submit_input, text="", fg=self.fg_color, bg=self.bg_colors[0])
+		error.pack()
+		#Button(submit_input, text="Save Configuration",command=save, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack(pady=5)
+		Button(submit_input, text="Export Sprites!",command=export, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack(pady=5)
+		
+
+class Editor(Frame):
+	def __init__(self, parent):
+		Frame.__init__(self, parent)
+		
+		
 
 class SplitterGUI():
-	
-	selected_name_index = -1
 
 	def __init__(self):
 		self.landing()
@@ -240,61 +355,8 @@ class SplitterGUI():
 			self.selected_name_index = name_list.index(name)
 			name_buttons[self.selected_name_index]["bg"] = select_color
 	
-		def on_sprite_click(row_index, col_index):
-			sprite = self.sprites[row_index][col_index]
-			if exclude_mode[0]:
-				sprite.exclude = not sprite.exclude
-				update_file_label(sprite)
-			elif delete_mode[0]:
-				sprite.names = []
-				update_file_label(sprite)
-			elif self.selected_name_index > -1:
-				sprite.names.append(name_list[self.selected_name_index])
-				update_file_label(sprite)
 		
-		def on_sprite_click_col(index):
-			if exclude_mode[0]:
-				exclude_all = True
-				for i in range(len(self.sprites)):
-					exclude_all = exclude_all and self.sprites[i][index].exclude
-				for i in range(len(self.sprites)):
-					self.sprites[i][index].exclude = not exclude_all
-					update_file_label(self.sprites[i][index])
-			else:
-				for i in range(len(self.sprites)):
-					on_sprite_click(i, index)
-	
-		def on_sprite_click_row(index):
-			if exclude_mode[0]:
-				exclude_all = True
-				for sprite in self.sprites[index]:
-					exclude_all = exclude_all and sprite.exclude
-				for sprite in self.sprites[index]:
-					sprite.exclude = not exclude_all
-					update_file_label(sprite)
-			else:
-				for i in range(len(self.sprites[0])):
-					on_sprite_click(index, i)
 		
-		def get_file_label(sprite):
-			if len(sprite.names) == 0:
-				return sprite.default
-			else: 
-				new_label = ""
-				for name in sprite.names:
-					new_label += name + delimiter_entry.get()
-				return new_label[:new_label.rindex(delimiter.get())]
-		
-		def update_file_label(sprite):
-			if sprite.exclude:
-				sprite.label["text"] = ""
-			else:
-				sprite.label["text"] = get_file_label(sprite)
-		
-		def update_file_labels():
-			for row in self.sprites:
-				for sprite in row:
-					update_file_label(sprite)
 		
 		def open_folder():
 			folder = fd.askdirectory(mustexist=True)
@@ -335,57 +397,7 @@ class SplitterGUI():
 		panel.pack(side=LEFT, fill=Y)
 		
 		
-		#Label(panel, text="Options", fg=self.fg_color, bg=self.bg_colors[0], font=("TkDefaultFont", 20) ).pack()
-		delimiter_input = Frame(panel, bg=self.bg_colors[0])
-		delimiter_input.pack(padx=5, pady=10)
-		mode_input = Frame(panel, bg=self.bg_colors[0])
-		mode_input.pack(padx=5, pady=5)
-		Label(panel, text="Names", fg=self.fg_color, bg=self.bg_colors[0], font=("TkDefaultFont", 14)).pack(padx=5, pady=5)
-		name_input = Frame(panel, bg=self.bg_colors[0])
-		name_input.pack(padx=5)
-		names_frame = Frame(panel, bg=self.bg_colors[0])
-		names_frame.pack(expand=True, fill=Y, padx=10, pady=5)
-		names_canvas = Canvas(names_frame, width=200, borderwidth=0, bg=self.bg_colors[1], scrollregion=(0,0,0,0))
-		names_vbar = Scrollbar(names_frame, orient=VERTICAL)
-		names_vbar.pack(side=RIGHT, fill=Y)
-		names_vbar.config(command=names_canvas.yview)
-		names_canvas.config(yscrollcommand=names_vbar.set)
-		names_canvas.bind("<MouseWheel>", on_names_mousewheel)
-		names_canvas.pack(expand=True, fill=Y)
-		names_panel = Frame(names_canvas, bg=self.bg_colors[1])
-		names_panel.pack(side=LEFT, padx=5, pady=10)
-		names_canvas.create_window((100,5), window=names_panel, anchor="n",tags="names_panel")
-		submit_input = Frame(panel, bg=self.bg_colors[0])
-		submit_input.pack(side=BOTTOM, padx=5, pady=10)
-		folder_input = Frame(panel, bg=self.bg_colors[0])
-		folder_input.pack(side=BOTTOM, padx=5)
 		
-		Label(delimiter_input, text="Delimiter:", fg=self.fg_color, bg=self.bg_colors[0]).pack(side=LEFT)
-		delimiter = StringVar(value="_")
-		delimiter.trace_add("write", lambda name, index, mode: update_file_labels())
-		delimiter_entry = Entry(delimiter_input, textvariable=delimiter, fg=self.fg_color, bg=self.bg_colors[2], width=4, relief=FLAT)
-		delimiter_entry.pack(side=LEFT)
-		
-		exclude_toggle = Button(mode_input, text="Exclude Mode", command=toggle_exclude_mode, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT)
-		exclude_toggle.pack(side=LEFT, padx=5)
-		delete_toggle = Button(mode_input, text="Clear Mode", command=toggle_delete_mode, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT)
-		delete_toggle.pack(side=LEFT, padx=5)
-		exclude_mode = [False, exclude_toggle]
-		delete_mode = [False, delete_toggle]
-		
-		name_entry = Entry(name_input, fg=self.fg_color, bg=self.bg_colors[2], relief=FLAT)
-		name_entry.bind('<Return>', add_name)
-		name_entry.pack(side=LEFT)
-		Button(name_input, text="Add Name",command=add_name, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack(side=LEFT, padx=5)
-		
-		folder_name = Label(folder_input, text="No directory selected.", fg=self.fg_color, bg=self.bg_colors[0], wraplength=180)
-		folder_name.pack()
-		Button(folder_input, text="Browse...",command=open_folder, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack()
-		
-		error = Label(submit_input, text="", fg=self.fg_color, bg=self.bg_colors[0])
-		error.pack()
-		#Button(submit_input, text="Save Configuration",command=save, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack(pady=5)
-		Button(submit_input, text="Export Sprites!",command=export, fg=self.fg_color, bg=self.bg_colors[1], relief=FLAT).pack(pady=5)
 		
 		
 		
